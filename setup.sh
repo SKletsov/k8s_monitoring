e3cbe1f8e1a1138e6d5bd81ba3f1d39bb74d3e34
Kletsov, Sergey <Sergey.Kletsov>
2021-01-28 23:43:58 +0300

    first commit

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#export KUBECONFIG=./k8s.conf
+# wordir=$(pwd)
+# KUBECONFIG=$wordir/inventory/my-cluster/artifacts/admin.conf
+#curl -L -O https://raw.githubusercontent.com/elastic/beats/7.10/deploy/kubernetes/filebeat-kubernetes.yaml
+#####
+#Helm Version: 3.3.4+
+#Kubernetes Version: 1.19.0
+####
+
+
+elk(){
+ export ELK_VERSION=7.9.2
+ helm repo add elastic https://helm.elastic.co
+ helm repo update
+ _elastic
+ _kibana
+ _filebeart
+}
+
+_elastic(){
+  #check https://kubernetes.io/docs/tasks/administer-cluster/change-default-storage-class/
+   helm install elasticsearch  elastic/elasticsearch --namespace default  --set imageTag=$ELK_VERSION --set replicas=1 --set esMajorVersion=7 --set resources.requests.memory=1Gi --set volumeClaimTemplate.storageClassName=hostpath --set volumeClaimTemplate.resources.requests.storage=10Gi
+}
+
+_kibana(){
+   helm install kibana elastic/kibana --namespace default  --set imageTag=$ELK_VERSION,elasticsearch.hosts=http://elasticsearch-master.default.svc.cluster.local:9200
+}
+
+_filebeart(){
+  kubectl apply -f filebeat-kubernetes.yaml 
+}
+
+
+
+prometeus(){
+  cd kube-prometheus
+  kubectl create ns monitoring
+  kubectl get servicemonitors
+  kubectl create -f manifests/setup
+  kubectl create -f manifests/
+}
+
+case "$1" in
+  prepare-cluster)  shift; prepare-cluster "$@" ;;
+  prometeus)  shift; prometeus "$@" ;;
+  elk)  shift; elk "$@" ;;
+  *) print_help; exit 1
+esac
